8f839df7d6c0cf95d618f323543f2d1aa978ab5d
Mfern10 <mitchellfernandez147@gmail.com>
2023-10-05 13:08:58 +0900

    Compare lesson finished

---
+++ python/control-flow/compare.py
@@ -0,0 +1,60 @@
+import math
+
+# x = int(input("What is X? "))
+# y = int(input("What is Y? "))
+
+# if x < y:
+#     print("x is less than y")
+# elif x > y: #else if 
+#     print("x is greater than y")
+# else:
+#     print("x is equal to y")
+
+
+# score = int(input("Score: "))
+
+# if score >= 90:
+#     print("HD")
+# elif score >= 80 and score < 90:
+#     print("D")
+# elif score >= 70 and score < 80:
+#     print("CR")
+# elif score >= 50 and score < 70:
+#     print("P")
+# else: 
+#     print("F")
+
+# name = input("What is your name?")
+
+# match name:
+#     case "Harry" | "Ron" | "Hermione":
+#         print("Gryffindor")
+#     case "Draco":
+#         print("Slytherin")
+#     case _: 
+#         print("Mudblood")
+
+
+# afternon challenge
+
+# shape = (input("Choose a shape, square, circle or triangle: "))
+
+
+# if shape =="square":
+#     print("you chose a square shape!")
+#     size = float(input("What is its edge length? "))
+#     surface = size ** 2
+#     print("The surface of a square is " + str(surface ))
+# elif shape =="circle":
+#     print("you chose a circle shape!")
+#     radius = float(input("What is its radius? "))
+#     area = math.pi * (radius ** 2)
+#     print("The area of a circle is " + str(area))
+# else:
+#     shape == "triangle"
+#     base =  float(input("What is its base? "))
+#     height = float(input("What is its height? "))
+#     area =  base * height / 2
+#     print("The triangles area size is " + str(area))
+
+
