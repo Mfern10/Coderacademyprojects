088a9d204b11e3b2cb466fbdc573001a0ce51935
Mfern10 <mitchellfernandez147@gmail.com>
2023-10-17 10:32:14 +0900

    Finished composition and inheritance

--- python/composition/main.py
+++ python/composition/main.py
@@ -1,21 +1,24 @@
 import rpg
 
-aragorn = rpg.Character("Aragorn", "Human")
-galadriel = rpg.Character("Galadriel", "Elf")
-frodo = rpg.Character("Frodo", "Hobbit")
+aragorn = rpg.Ranger("Aragorn", "Human", 100, 50)
+galadriel = rpg.Mage("Galadriel", "Elf", 120, 75)
+frodo = rpg.Burglar("Frodo", "Hobbit", 50, 25)
+saruman = rpg.Wizard("Saruman", "Human", 80, 100)
 
 frodo.inv.set_currency(9, 47, 23)
 
 chest = rpg.Chest(['Longsword', 'iron helm'], 2, 25, 50)
 
+galadriel.battle(aragorn)
+
 # print(chest.inv.__dict__)
 # print(aragorn.__dict__)
-# print(galadriel.__dict__)
+print(galadriel.__dict__)
 # print(frodo.__dict__)
 
 #frodo loot chest!
-chest.inv.transfer(frodo.inv)
+# chest.inv.transfer(frodo.inv)
 
-print(frodo.inv.__dict__)
-print(frodo.inv.get_currency())
-print(chest.inv.__dict__)
+# print(frodo.inv.__dict__)
+# print(frodo.inv.get_currency())
+# print(chest.inv.__dict__)
--- python/composition/rpg.py
+++ python/composition/rpg.py
@@ -1,9 +1,43 @@
 class Character:
-    def __init__(self, name, race):
+    def __init__(self, name, race, attack, health):
         self.name = name
         self.race = race
-        self.copper = 0
+        self.attack = attack
+        self.health = health
         self.inv = Inventory([], 0, 0, 0)
+
+    def battle(self, other):
+        print(f"{self.name} attacks {other.name}!")
+
+class Ranger(Character):
+    def battle(self, other):
+        print(f"{self.name} launches a brutal melee attack on {other.name}!")
+
+    def recruit_undead(self):
+        pass
+
+class Mage(Character):
+    def __init__(self, name, race, attack, health):
+        # Call the superclass constructor
+        super().__init__(name, race, attack, health)
+        self.mana = 100
+
+
+    def battle(self, other):
+        print(f"{self.name} Casts a wicked spell at {other.name}!")
+        self.mana -= 20
+
+class Burglar(Character):
+    def battle(self, other):
+        print(f"{self.name} sneaks in a stealth attack on {other.name}!")
+
+class Wizard(Character):
+    def battle(self, other):
+        print(f"{self.name} summons and orc minion, which attacks {other.name}!")
+           
+
+
+
 
 class Chest:
     def __init__(self, items, gold, silver, copper):
@@ -13,7 +47,6 @@ class Inventory:
     def __init__(self, items, gold, silver, copper):
         self.items = items
         self.set_currency(gold, silver, copper) #Delegation
-
 
     #from_inv and to_inv are instances of Inventory
     def transfer(self, to_inv):
