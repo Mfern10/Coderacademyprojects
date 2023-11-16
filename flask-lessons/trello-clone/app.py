e2b3a4e4f98c0e43c83098996756fdfd8c155487
Mfern10 <mitchellfernandez147@gmail.com>
2023-11-16 12:01:59 +0900

    Finished ORM2 lesson, marshmallow and json files

--- flask-lessons/trello-clone/app.py
+++ flask-lessons/trello-clone/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date
+from flask_marshmallow import Marshmallow
+
 
 app = Flask(__name__)
 
@@ -8,14 +10,22 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://trello_dev:spameggs123@127.0.0.1:5432/trello'
 
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+class CardSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'title', 'description', 'status', 'date_created')
 
 # creates a model which represents a entity in the database/creates a table in postgres DB trello
 class Card(db.Model):
     __tablename__ = 'cards' # sets the table name to follow convention
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     description = db.Column(db.Text())
+    status = db.Column(db.String(30))
     date_created = db.Column(db.Date())
+
 
 # adds a terminal function to run to create the table
 @app.cli.command('db_create')
@@ -27,17 +37,43 @@ def db_create():
 # creates a terminal function to run that places data into the table
 @app.cli.command('db_seed')
 def db_seed():
-    card = Card(
-        title = 'Start the project',
-        description = 'Stage 1 - ERD Creation',
-        date_created = date.today()
-    )
+    cards= [
+        Card(
+            title = 'Start the project',
+            description = 'Stage 1 - ERD Creation',
+            status = 'Done',
+            date_created = date.today()
+        ),
+        Card(
+            title = 'ORM Queries',
+            description = 'Stage 2 - Implement CRUD queries',
+            status = 'In progress',
+            date_created = date.today()
+        ),
+        Card(
+            title = 'Marshmallow',
+            description = 'Stage 3 - Implement JSONify of meodels',
+            status = 'In progress',
+            date_created = date.today()
+        ),
+    ]
     # adds the data then commits it to the database will now show in psql database
-    db.session.add(card)
+    db.session.add_all(cards)
     db.session.commit()
 
     print('Database seeded')
 
+# selects all cards in a function and prints them out as a dictionary
+@app.route('/cards')
+def all_cards():
+    # select * from cards;
+    stmt = db.select(Card).where(db.or_(Card.status != 'Done', Card.id > 2)).order_by(Card.title.desc())
+    cards = db.session.scalars(stmt).all()
+    return CardSchema(many=True).dump(cards)
+
+
+
 @app.route('/')
 def index():
     return '<h3>Hello, World!</h3>'
+
