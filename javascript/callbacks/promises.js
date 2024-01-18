0c57cf74f87f2c60181632036b486a6c18ab270b
Mfern10 <mitchellfernandez147@gmail.com>
2024-01-18 11:25:16 +0900

    async/await and modules done

--- javascript/callbacks/promises.js
+++ javascript/callbacks/promises.js
@@ -1,35 +1,36 @@
 const x = 2;
-const y = "dfgdfg";
+const y = "dfgdfgd";
 
 function adder(a, b) {
   return a + b;
 }
 
-function adderPromise(x, y) {
-  return new Promise((resolve, reject) => {
-    if (typeof x === "number" && typeof y === "number") {
-      const answer = adder(x, y);
-      resolve(answer);
-    } else {
-      reject("Both x and y must be numbers");
-    }
-  });
+async function adderPromise(x, y) {
+  if (typeof x === "number" && typeof y === "number") {
+    const answer = adder(x, y);
+    return answer;
+  } else {
+    throw "x and y must be a number";
+  }
 }
 
 // adderPromise(10, 20)
-//   .then((value) =>  {
-//     adderPromise(value, 100)
-//     .then(answer => console.log(answer))
-//   })
-//   .catch((err) => console.error(err));
+//     .then(value => {
+//         adderPromise(value, 100)
+//             .then(answer => console.log(answer))
+//     })
+//     .catch(err => console.error(err))
 
-adderPromise(10, 20)
-  .then((value) => adderPromise(value, 100))
-  .then(answer => console.log(answer))
-  .catch((err) => console.error(err));
+async function doStuff() {
+  const value = await adderPromise(10, 20);
+  console.log(value);
+}
+// .then(value => adderPromise(value, 100))
+// .then(answer => console.log(answer))
+// .catch(err => console.error(err))
 
 // adderPromise(3.7, 123.45)
-//   .then((value) => alert(value))
-//   .catch((err) => console.error(err));
-
+//     .then(value => alert(value))
+//     .catch(err => console.error(err))
+doStuff();
 console.log("Not waiting for resolve!");
--- javascript/callbacks/script.js
+++ javascript/callbacks/script.js
@@ -1,78 +1,67 @@
-// EXAMPLE OF CALLBACK FUNCTION.
-// function adder(x, y, cb) {
-//     cb(x + y)
-// }
-
-// adder(5, 10, result => console.log(result))
-
-// console.log('Done')
-
-// SENDING REQUESTS TO THE SERVER OR API
-// -OLD WAY
-// function getJoke() {
-//   return new Promise((resolve, reject) => {
-//     const req = new XMLHttpRequest();
-//     // set event listener for the request
-//     req.addEventListener("load", (event) =>
-//       resolve(event.target.response.joke)
-//     );
-//     // open the request
-//     req.open("GET", "https://icanhazdadjoke.com/");
-//     // set the request header
-//     req.setRequestHeader("Accept", "application/json");
-//     // set respomse type
-//     req.responseType = "json";
-//     // send the request
-//     req.send();
-//   });
-// }
-
-function fetchJoke() {
-    fetch('https://icanhazdadjoke.com', {
-        headers: {
-            'Accept': 'application/json'
-        }
-    }).then(res => res.json()).then(data => console.log(data))
+function getJoke() {
+  return new Promise((resolve, reject) => {
+    try {
+      // 2. Create XHR
+      const req = new XMLHttpRequest();
+      // 3. Add listener for when the response is received and parsed
+      // 9. Listener callback is triggered, which in turn calls cb, passing the joke
+      req.addEventListener("load", (event) =>
+        resolve(event.target.response.joke)
+      );
+      // 4. Open the URL with the appropriate verb
+      req.open("GET", "https://icanhazdadjoke.com/");
+      // 5. Set Accept header so server gives us JSON
+      req.setRequestHeader("Accept", "application/json");
+      // 6. Set responeType so XHR object parses the JSON
+      req.responseType = "json";
+      // 7. Send the request, then immediately return from getJoke (i.e. don't wait!)
+      req.send();
+    } catch (e) {
+      reject(e);
+    }
+  });
 }
 
-fetchJoke()
+async function fetchJoke() {
+  const res = await fetch("https://icanhazdadjoke.com/", {
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  const data = await res.json();
+  return data.joke;
+  // .then(res => res.json())
+  // .then(data => console.log(data))
+}
 
+fetchJoke().then((joke) => console.log(joke));
+
+// 1. Call getJoke and pass a callback function
+// 10. Callback is called via cb in the load listener in getJoke, receiving the joke
 // const jokes = []
-// getJoke()
-//   .then((joke) => {
-//     jokes.push(joke);
-//     return getJoke();
-//   })
-//   .then((joke) => {
-//     jokes.push(joke);
-//     return getJoke();
-//   })
-//   .then((joke) => {
-//     jokes.push(joke);
-//     console.log(jokes);
-//   })
 
-// const jokePromises = [];
+// getJoke()
+//     .then(joke => {
+//         jokes.push(joke)
+//         return getJoke()
+//     })
+//     .then(joke => {
+//         jokes.push(joke)
+//         return getJoke()
+//     })
+//     .then(joke => {
+//         jokes.push(joke)
+//         console.log(jokes)
+//     })
+
+// const jokePromises = []
 // for (let i = 0; i < 3; i++) {
-//   jokePromises.push(getJoke());
+//     jokePromises.push(getJoke())
 // }
 
 // Promise.all(jokePromises)
-//   .then((jokes) => console.log(jokes))
-//   .catch((err) => console.error(err));
+//     .then(jokes => console.log(jokes))
+//     .catch(err => console.error(err))
 
-console.log("Request sent");
-
-// const req = new XMLHttpRequest()
-// // set event listener for the request
-// req.addEventListener('load', event => cb(event.target.response.joke))
-// // open the request
-// req.open('GET', 'https://icanhazdadjoke.com/')
-// // set the request header
-// req.setRequestHeader('Accept', 'application/json')
-// // set respomse type
-// req.responseType = 'json'
-// // send the request
-// req.send()
-// }
-// call get joke function and the callback to send the joke.
+// 8. getJoke returned immediately, so log out message
+console.log("Request sent!");
