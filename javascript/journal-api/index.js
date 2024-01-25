f0f3ef76fb1aa6145e724d21b0859339765b9348
Mfern10 <mitchellfernandez147@gmail.com>
2024-01-25 13:40:37 +0900

    Completed CRUD operations or categories and seperated categories into its own routes file created router

--- javascript/journal-api/index.js
+++ javascript/journal-api/index.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { CategoryModel } from "./db.js";
-import entryRoutes from './routes/entry_routes.js'
+import categoryRoutes from "./routes/category_routes.js";
+import entryRoutes from "./routes/entry_routes.js";
 
 const app = express();
 
@@ -9,20 +9,16 @@ const app = express();
 // use lets us use the middlewear makes sure items come through as JSON
 app.use(express.json());
 
-// TODO: Move /categories to routes folder and modularize
-// TODO: Complete Categories CRUD
-// TODO: ADVANCED: Modify "GET /categories/:id" to embed an array of all entries in that category
-
-
 // creating a route in express. HTTP METHODS ARE USED AFTER DOT
 app.get("/", (req, res) => res.send({ info: "Journal API" })); // sends response as JSON
 
-// creates route to fetch categories. CREATE FULL CRUD ROUTES
-app.get("/categories", async (req, res) =>
-  res.status(201).send(await CategoryModel.find())
-);
+// TODO: Move /categories to routes folder and modularize - DONE
+// TODO: Complete Categories CRUD -
+// TODO: ADVANCED: Modify "GET /categories/:id" to embed an array of all entries in that category
 
-app.use('/entries', entryRoutes);
+app.use("/categories", categoryRoutes);
+
+app.use("/entries", entryRoutes);
 
 app.listen(4002, () => {
   console.log("Server is running on http://127.0.0.1:4001");
